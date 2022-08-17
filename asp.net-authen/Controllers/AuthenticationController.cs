7ee3537dc5bbb96fc5b94e4bb551e10bacfab09d
Jirayu Ch <52649379+TdotShare@users.noreply.github.com>
2022-08-17 16:22:36 +0700

    Initial commit

---
+++ asp.net-authen/Controllers/AuthenticationController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net_authen.Controllers
+{
+
+    [Route("auth")]
+    public class AuthenticationController : Controller
+    {
+        [Route("login", Name = "login_index_page")]
+        public IActionResult Index()
+        {
+            return View("Views/Login/Index.cshtml");
+        }
+
+
+
+        [HttpPost("login_data", Name = "auth_login_data")]
+        public IActionResult actionLogin()
+        {
+
+            Dictionary<string, string> data = new Dictionary<string, string>();
+
+            foreach (var key in Request.Form.Keys)
+            {
+                data.Add(key, Request.Form[key]);
+            }
+
+
+            if (data["username"] == "admin" && data["password"] == "admin")
+            {
+                HttpContext.Session.SetInt32("auth", 1);
+                return RedirectToRoute("home_index_page");
+            }
+            else
+            {
+                actionAlertData("login failed", "warning");
+                return Redirect(HttpContext.Request.Headers["Referer"]);
+            }
+        }
+
+
+
+        [Route("logout", Name = "login_clear_data")]
+        public IActionResult actionLogout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToRoute("login_index_page");
+        }
+
+        public void actionAlertData(string msg, string status = "success")
+        {
+            // success , danger , warning
+            TempData["alear"] = "true";
+            TempData["msg"] = msg;
+            TempData["status"] = status;
+        }
+
+    }
+}
---
+++ asp.net-authen/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using asp.net_authen.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace asp.net_authen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("")]
+        [Route("home" , Name = "home_index_page")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
